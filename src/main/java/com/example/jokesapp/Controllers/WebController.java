c8f8f697f866e7061023b2ddca9128f296202caf
AdamClayton98 <u1764481@unimail.hud.ac.uk>
2021-06-11 19:46:20 +0100

    Joke categories working

--- src/main/java/com/example/jokesapp/Controllers/WebController.java
+++ src/main/java/com/example/jokesapp/Controllers/WebController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.xml.crypto.Data;
-import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 @Controller
@@ -23,6 +21,27 @@ public class WebController {
     public String home(Model model) throws SQLException {
         databaseMethods=new DatabaseMethods();
         model.addAttribute("jokes", databaseMethods.getAllJokes(jdbcTemplate));
+        return "home";
+    }
+
+    @GetMapping("/general")
+    public String general(Model model) throws SQLException {
+        databaseMethods=new DatabaseMethods();
+        model.addAttribute("jokes", databaseMethods.getGeneralJokes(jdbcTemplate));
+        return "home";
+    }
+
+    @GetMapping("/knockknock")
+    public String knockknock(Model model) throws SQLException {
+        databaseMethods=new DatabaseMethods();
+        model.addAttribute("jokes", databaseMethods.getKnockKnockJokes(jdbcTemplate));
+        return "home";
+    }
+
+    @GetMapping("/programming")
+    public String programming(Model model) throws SQLException {
+        databaseMethods=new DatabaseMethods();
+        model.addAttribute("jokes", databaseMethods.getProgrammingJokes(jdbcTemplate));
         return "home";
     }
 
